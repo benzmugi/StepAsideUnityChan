3d97dbb041ebb847000a56a05bfb7a8901ec600b
benzmugi <mugiyuiyui@gmail.com>
2019-06-10 10:05:02 +0900

    課題提出

---
+++ Assets/Destroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    private GameObject unitychan;
+    
+    private float difference;
+
+
+    // Use this for initialization
+    void Start () {
+        this.unitychan = GameObject.Find("unitychan");
+        
+        this.difference = unitychan.transform.position.z - Camera.main.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (this.transform.position.z < this.unitychan.transform.position.z - this.difference)
+        {
+            Destroy(this.gameObject);
+        }
+
+       
+	}
+}
